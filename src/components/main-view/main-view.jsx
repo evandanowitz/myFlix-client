cceada18baea3b939c911ae3fca2eb9ce1f918f1
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2023-12-12 16:23:41 -0500

    create/export "MainView" component
    Manually create movie objects

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,76 @@
+
+export const MainView = () => {
+  const [movies, setMovies] = useState([
+    { 
+      id: 1, 
+      title: "The Dark Knight", 
+      description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.", 
+      genre: "Action",
+      director: "Christopher Nolan",
+      image: "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_FMjpg_UX1000_.jpg", 
+      featured: true
+    },
+    { 
+      id: 2, 
+      title: "Inception", 
+      description: "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.", 
+      genre: "Action",
+      director: "Christopher Nolan",
+      image: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_FMjpg_UX1000_.jpg", 
+      featured: true
+    },
+    { 
+      id: 3, 
+      title: "The Silence of the Lambs", 
+      description: "A young F.B.I. cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer, a madman who skins his victims.", 
+      genre: "Thriller",
+      director: "Jonathan Demme",
+      image: "https://m.media-amazon.com/images/M/MV5BNjNhZTk0ZmEtNjJhMi00YzFlLWE1MmEtYzM1M2ZmMGMwMTU4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_FMjpg_UX1000_.jpg", 
+      featured: true
+    },
+    { 
+      id: 4, 
+      title: "Parasite", 
+      description: "Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.", 
+      genre: "Thriller", 
+      director: "Bong Joon Ho",
+      image: "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_UY1200_CR90,0,630,1200_AL_.jpg", 
+      featured: false
+    },
+    { 
+      id: 5, 
+      title: "The Departed", 
+      description: "An undercover cop and a mole in the police attempt to identify each other while infiltrating an Irish gang in South Boston.", 
+      genre: "Thriller", 
+      director: "Martin Scorsese",
+      image: "https://m.media-amazon.com/images/M/MV5BMTI1MTY2OTIxNV5BMl5BanBnXkFtZTYwNjQ4NjY3._V1_FMjpg_UX1000_.jpg", 
+      featured: false
+    }
+  ]);
+
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  if (selectedMovie) {
+    return (
+      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+    );
+  }
+
+  if (movies.length === 0) {
+    return <div>The list is empty!</div>;
+  } else {
+  
+  return (
+    <div>
+      {movies.map((movie) => (
+        <MovieCard 
+          key={movie.id} 
+          movie={movie} 
+          onMovieClick={(newSelectedMovie) => {
+            setSelectedMovie(newSelectedMovie);
+          }}
+        />
+      ))}
+    </div>
+  );
+}};
