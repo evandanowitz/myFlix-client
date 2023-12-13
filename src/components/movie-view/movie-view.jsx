bc33bd060c7b0bcb17918c7fa88123513bc8e5a2
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2023-12-13 17:05:32 -0500

    add ternary operator to display "featured"

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -25,6 +25,7 @@ export const MovieView = ({ movie, onBackClick }) => {
       <div>
         <span>Featured: </span>
         <span>{movie.featured}</span>
+        <span>{movie.featured ? "Yes" : "No"}</span>
       </div>
 
       <button onClick={onBackClick}>Back</button>
