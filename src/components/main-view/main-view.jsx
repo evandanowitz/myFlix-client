fac77aacf8786f1d5b9a0b199d83a5dd8ac00c7f
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2023-12-21 17:38:27 -0500

    Load data from "movies" array from API

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -6,6 +6,34 @@ import { MovieView } from "../movie-view/movie-view";
 export const MainView = () => {
   const [movies, setMovies] = useState([]);
 
+  useEffect(() => {
+    fetch("https://myflix-db-movie-app-af5513e7733f.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const moviesFromApi = data.map((movie) => {
+          return {
+            _id: movie._id,
+            Title: movie.Title,
+            Description: movie.Description,
+            Genre: {
+              Name: movie.Genre.Name,
+              Description: movie.Genre.Description
+            },
+            Director: {
+              Name: movie.Director.Name,
+              Bio: movie.Director.Bio,
+              Birth: movie.Director.Birth,
+              Death: movie.Director.Death
+            },
+            ImagePath: movie.ImagePath,
+            Featured: movie.Featured
+          };
+        });
+        setMovies(moviesFromApi);
+      });
+  }, []);
+
   const [selectedMovie, setSelectedMovie] = useState(null);
 
   if (selectedMovie) {
@@ -22,7 +50,7 @@ export const MainView = () => {
     <div>
       {movies.map((movie) => (
         <MovieCard 
-          key={movie.id} 
+          key={movie._id} 
           movie={movie} 
           onMovieClick={(newSelectedMovie) => {
             setSelectedMovie(newSelectedMovie);
