2337036762cc39458717bf937bd9cb97a416bcb2
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-01-31 15:48:36 -0500

    gitignore

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -28578,7 +28578,7 @@ $RefreshReg$(_c, "LoginView");
   window.$RefreshReg$ = prevRefreshReg;
   window.$RefreshSig$ = prevRefreshSig;
 }
-},{"react/jsx-dev-runtime":"iTorj","@parcel/transformer-js/src/esmodule-helpers.js":"8fMv8","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"fkdcD","react":"21dqq"}],"4OGiN":[function(require,module,exports) {
+},{"react/jsx-dev-runtime":"iTorj","react":"21dqq","@parcel/transformer-js/src/esmodule-helpers.js":"8fMv8","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"fkdcD"}],"4OGiN":[function(require,module,exports) {
 var $parcel$ReactRefreshHelpers$73d1 = require("@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js");
 var prevRefreshReg = window.$RefreshReg$;
 var prevRefreshSig = window.$RefreshSig$;
