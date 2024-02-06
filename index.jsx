85deb0e6d1df6657dc1229a9c61f90f485f52f85
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-02-06 12:39:20 -0500

    Commit all changes to merge to main
    This is the solution to the merge and revert issue.
    
    Created new branch based on main, removed dist, added to gitignore, and copied all other files over.
    
    Added LoginView and SignupView components

---
+++ index.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+import { MainView } from "./components/main-view/main-view";
+import "./index.scss";
+
+const MyFlixApp = () => {
+  return <MainView />;
+};
+
+const container = document.querySelector("#root");
+const root = createRoot(container);
+root.render(<MyFlixApp />);
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,15 +2,29 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card"; // This is how you import one component into another
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser? storedUser : null);
+  const [token, setToken] = useState(storedToken? storedToken : null);
   const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
-    fetch("https://myflix-db-movie-app-af5513e7733f.herokuapp.com/movies")
+    if (!token) {
+      return;
+    }
+
+    fetch("https://myflix-db-movie-app-af5513e7733f.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${token}` },
+    })
       .then((response) => response.json())
       .then((data) => {
         console.log("movie data", data);
+
         const moviesFromApi = data.map((movie) => {
           return {
             _id: movie._id,
@@ -32,22 +46,42 @@ export const MainView = () => {
         });
         setMovies(moviesFromApi);
       });
-  }, []);
+  }, [token]);
 
-  const [selectedMovie, setSelectedMovie] = useState(null);
+  if (!user) {
+    return (
+      <>
+        <LoginView onLoggedIn={(user, token) => {
+          setUser(user);
+          setToken(token);
+        }} />
+        or
+        <SignupView />
+      </>
+    );
+  }
 
   if (selectedMovie) {
     return (
-      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+      <div>
+        <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
+        <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+      </div>
     );
   }
 
   if (movies.length === 0) {
-    return <div>The list is empty!</div>;
+    return (
+      <div>
+        <div>The list is empty!</div>
+        <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
+      </div>
+    );
   } else {
   
   return (
     <div>
+      <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
       {movies.map((movie) => (
         <MovieCard 
           key={movie._id} 
