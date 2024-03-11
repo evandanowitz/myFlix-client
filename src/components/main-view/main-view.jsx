85fbcb8b731e46766c38f29a4b2823bf79c9d508
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-03-11 18:45:56 -0400

    Remove spacing

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -132,7 +132,7 @@ export const MainView = () => {
                   </Col>
                 )}
               </>
-            } />
+            } />    
           <Route 
             path="/"
             element={
