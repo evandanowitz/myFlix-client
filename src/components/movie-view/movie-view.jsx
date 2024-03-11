e75e54133a7df66d199c98b4c5db53cad2c24fa6
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-03-11 18:36:33 -0400

    Create rmvFromFavorites function

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -47,6 +47,41 @@ export const MovieView = ({ movies }) => {
     })
   };
 
+  const rmvFromFavorites = (event) => {
+    event.preventDefault();
+    const token = localStorage.getItem("token");
+    if (!movie || !Username) {
+      console.error("Selected movie or Username not found");
+      return;
+    }
+    fetch(`https://myflix-db-movie-app-af5513e7733f.herokuapp.com/users/${Username}/movies/${movie._id}`, {
+      method: "DELETE",
+      headers: { 
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+      }
+    }).then((response) => {
+        if (!response) {
+          console.error("No response received");
+          return;
+        }
+        if (!response.ok) {
+          console.error(`HTTP error! Status: ${response.status}`);
+          return;
+        }
+        return response.json();
+      }).then((data) => {
+        localStorage.setItem("user", JSON.stringify(data));
+        setIsFavorite(false); // Update the state after a successful API response
+        if (data && data.Username) {
+          onFavMoviesChange();
+          alert("Movie REMOVED from Favorites List");
+          // userCallback(data); // Pass the updated user data to the parent component
+        }
+      }).catch((error) => {
+      console.error("Error adding movie to favorites:", error);
+    })
+  };
 
   return (
     <>
