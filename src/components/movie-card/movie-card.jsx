7d6572de5e7b44422de2f68dc9eec137c4f5caf9
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2024-03-21 18:16:41 -0400

    Update component files

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,30 +1,19 @@
 import PropTypes from "prop-types";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-
+import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "./movie-card.scss";
 
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie }) => {
   return (
-    <Card className="h-100" onClick={() => onMovieClick(movie)}>
+    <Card className="h-100" md={6}>
       <Card.Img variant="top" src={movie.ImagePath} />
       <Card.Body className="d-flex flex-column">
-        <Row>
-          <Col>
-            <Card.Title>{movie.Title}</Card.Title>
-            <Card.Text>{movie.Description}</Card.Text>
-            <Card.Text>{movie.Genre.Name}</Card.Text>
-          </Col>
-        </Row>
-        <Row className="mt-auto">
-          <Col>
-            <Button onClick={() => onMovieClick(movie)} variant="link">
-              View More
-            </Button>
-          </Col>
-        </Row>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Text>{movie.Description}</Card.Text>
+        <Card.Text>{movie.Genre.Name}</Card.Text>
+        <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
+          <Button variant="link" className="mt-auto">Open</Button>
+        </Link>
       </Card.Body>
     </Card>
   );
@@ -33,6 +22,5 @@ export const MovieCard = ({ movie, onMovieClick }) => {
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string
-  }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+  }).isRequired
 };
